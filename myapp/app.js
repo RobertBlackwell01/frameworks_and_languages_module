804a58baa99b7eb8d88a104fa13bb66c39415256
RobertBlackwell01 <91064335+RobertBlackwell01@users.noreply.github.com>
2023-11-17 19:04:51 +0000

    introduction of express JS to build RESTAPI

---
+++ myapp/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = 8000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+  // Docker container exit handler - https://github.com/nodejs/node/issues/4182
+  process.on('SIGINT', function() {process.exit()})
